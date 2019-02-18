dea1dbaf2c54e2e76c76de232a71959582195293
rocsteady888 <rocsteady888@gmail.com>
2019-02-18 10:54:13 -0500

    changed folder structer for MVC model

--- server/src/routes/api/users.js
+++ server/src/routes/api/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const bcrypt = require('bcryptjs');
 
 // Load User model
 const User = require('../../models/User');
@@ -23,8 +23,46 @@ router.post('/register', (req, res) => {
           name: req.body.name,
           email: req.body.email,
           password: req.body.password
-        })
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if(err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err))
+          })
+        });
       }
+    })
+});
+
+// @route   GET api/users/login
+// @desc    Login User / Return JWT Token
+// @access  Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({email})
+    .then(user => {
+      // Check for User
+      if(!user) {
+        return res.status(404).json({email: 'Email not found'});
+      }
+
+      // Check Password
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            res.json({msg: 'Success'})
+          } else {
+            return res.status(400).json({password: 'Password incorrect'});
+          }
+        })
     })
 });
 
