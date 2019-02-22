7a495564f10887a0a4fa4b0a605dbfc99658ca19
rocsteady888 <rocsteady888@gmail.com>
2019-02-22 14:42:19 -0500

    added unit preference to user model

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -15,6 +15,10 @@ const UserSchema = new Schema({
     type: String,
     required: true
   },
+  siUnits: {
+    type: Boolean,
+    default: false
+  },
   date: {
     type: Date,
     default: Date.now
