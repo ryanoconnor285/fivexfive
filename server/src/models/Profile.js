72d508ce4aed93cc9ec71b2b29283b2e771ab32b
rocsteady888 <rocsteady888@gmail.com>
2019-02-22 18:08:46 -0500

    initial commit

---
+++ server/src/models/Profile.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  currentWeight: {
+    type: Number,
+    default: 150
+  },
+  siUnits: {
+    type: Boolean,
+    default: false
+  },
+  deadlift: {
+    type: Number,
+    default: 45
+  },
+  benchpress: {
+    type: Number,
+    default: 45
+  },
+  squat: {
+    type: Number,
+    default: 45
+  },
+  shoulderPress: {
+    type: Number,
+    default: 45
+  },
+  row: {
+    type: Number,
+    default: 45
+  }
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema); 
