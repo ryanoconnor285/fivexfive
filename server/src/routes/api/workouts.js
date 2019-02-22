69ce78d9685f5b8e9074e9b8c9e51eb43926abf7
rocsteady888 <rocsteady888@gmail.com>
2019-02-22 18:11:21 -0500

    removed validation added routes to get and create workouts

--- server/src/routes/api/workouts.js
+++ server/src/routes/api/workouts.js
@@ -6,43 +6,50 @@ const passport = require('passport');
 // Load Workout Model
 const Workout = require('../../models/Workout');
 
-// Validation
-const validateWorkoutInput = require('../../validation/workout');
+// Load Workout Model
+const Profile = require('../../models/Profile');
+
+// Load User Model
+const User = require('../../models/User');
+
 
 // @route   GET api/workouts/test
 // @desc    Tests workout route
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: "Workouts Works" }));
 
+// @route   GET api/workouts
+// @desc    Get current user's workouts
+// @access  Private
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const errors = {};
+  Workout.find({ user: req.user.id })
+    .then(workouts => {
+      if (!workouts) {
+        errors.workouts = 'There are no workouts for this user';
+        return res.status(400).json(errors);
+      }
+      res.json(workouts);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
 // @route   POST api/workouts
-// @desc    Track a workout
+// @desc    Create workout
 // @access  Private
 router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
-  const { errors, isValid } = validateWorkoutInput(req.body);
-
-  // Check Validation
-  if(!isValid) {
-    // If any errors, send 400 with erros object
-    return res.status(400).json(errors);
-  }
-
-  // Get current weights for each exercise
-  const exercises = [];
-  if (req.body.deadlift) exercises.deadlift = req.body.deadlift;
-  if (req.body.benchpress) exercises.benchpress = req.body.benchpress;
-  if (req.body.squat) exercises.squat = req.body.squat;
-  if (req.body.shoulderPress) exercises.shoulderPress = req.body.shoulderPress;
-  if (req.body.row) exercises.row = req.body.row;
-
-  const newWorkout = new Workout({
-    user: req.user.id,
-    title: req.body.title,
-    exercises: exercises
-  });
-
-  newWorkout.save()
+  const workoutFields = {};
+  workoutFields.user = req.user.id;
+  if (req.body.currentWeight) workoutFields.currentWeight = req.body.currentWeight;
+  if (req.body.deadlift) workoutFields.deadlift = req.body.deadlift;
+  if (req.body.benchpress) workoutFields.benchpress = req.body.benchpress;
+  if (req.body.squat) workoutFields.squat = req.body.squat;
+  if (req.body.shoulderPress) workoutFields.shoulderPress = req.body.shoulderPress;
+  if (req.body.row) workoutFields.row = req.body.row;
+  
+  new Workout(workoutFields).save()
     .then(workout => res.json(workout))
-    .catch(err => res.json(err));
+
 });
 
 module.exports = router;
