c1936bc69c6e9da44ed846fd4705959d25ce5b2b
rocsteady888 <rocsteady888@gmail.com>
2019-02-27 13:50:10 -0500

    imported materialize, loging and register and added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+
 import Dashboard from './components/landing/Dashboard';
 import Navbar from './components/common/Navbar';
 import Footer from './components/common/Footer';
-import Workout from './components/workout/Workout';
+import Workout from './components/workouts/Workout';
 import Progress from './components/progress/Progress';
 import Discover from './components/discover/Discover';
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './App.css';
 
@@ -17,9 +22,14 @@ class App extends Component {
         <div className="App">
           <Navbar />
           <Route exact path="/" component={Dashboard} />
-          <Route path="/workout" component={Workout} />
-          <Route path="/progress" component={Progress} />
-          <Route path="/discover" component={Discover} />
+          <div className="container">
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+
+            <Route path="/workouts" component={Workout} />
+            <Route path="/progress" component={Progress} />
+            <Route path="/discover" component={Discover} />
+          </div>
 
           <Footer />
         </div>
