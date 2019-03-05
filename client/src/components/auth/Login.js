7ee9c7e8c21b966c26814ec4d66fbd526b5de5f0
rocsteady888 <rocsteady888@gmail.com>
2019-03-05 18:28:18 -0500

    changed spacing

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { loginUser } from '../../actions/authActions'
+import { loginUser } from '../../actions/authActions';
+
 class Login extends React.Component {
   constructor(props) {
     super(props);
