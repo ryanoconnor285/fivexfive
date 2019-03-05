7b14bda3fbf7381886768d5c02f8e09b5b55c98b
rocsteady888 <rocsteady888@gmail.com>
2019-03-05 18:30:02 -0500

    brought in proptypes to redirect unauthenticated users

--- client/src/components/landing/Dashboard.js
+++ client/src/components/landing/Dashboard.js
@@ -2,58 +2,79 @@ import React from 'react';
 import image1 from '../../images/pexels-photo-669576.jpeg';
 import image2 from '../../images/sports-fitness-body-building-iron-161557.jpeg';
 import image3 from '../../images/pexels-photo-416717.jpeg';
-import { Link } from 'react-router-dom';
 
-const Dashboard = () => {
-  return (
-    <div>  
-      <div className="row">
-        <div className="col m12 l4">
-          <div className="card">
-            <div className="card-image">
-              <img src={image2} alt="weight plates"/>
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class Dashboard extends React.Component {
+
+  componentDidMount() {
+    if (!this.props.auth.isAuthenticated) {
+      this.props.history.push('/');
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col m12 l4">
+            <div className="card">
+              <div className="card-image">
+                <img src={image2} alt="weight plates" />
                 <span className="card-title">Start a Workout</span>
-            </div>
-            <div className="card-content">
-              <p>Continue where you left off.  Weight and progress will be tracked automatically.</p>
-            </div>
-            <div className="card-action">
-              <Link to="/workouts">New Workout</Link>
-            </div>
-          </div>
-        </div>
-        <div className="col m12 l4">
-          <div className="card">
-            <div className="card-image">
-              <img src={image1} alt="weight plates" />
-              <span className="card-title">View Progress</span>
-            </div>
-            <div className="card-content">
-              <p>View your strength gains over time.  Look back at recorded workouts.</p>
-            </div>
-            <div className="card-action">
-              <Link to="/progress">View Progress</Link>
+              </div>
+              <div className="card-content">
+                <p>Continue where you left off.  Weight and progress will be tracked automatically.</p>
+              </div>
+              <div className="card-action">
+                <Link to="/workouts">New Workout</Link>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="col m12 l4">
-          <div className="card">
-            <div className="card-image">
-              <img src={image3} alt="weight plates" />
-              <span className="card-title">Discover</span>
+          <div className="col m12 l4">
+            <div className="card">
+              <div className="card-image">
+                <img src={image1} alt="weight plates" />
+                <span className="card-title">View Progress</span>
+              </div>
+              <div className="card-content">
+                <p>View your strength gains over time.  Look back at recorded workouts.</p>
+              </div>
+              <div className="card-action">
+                <Link to="/progress">View Progress</Link>
+              </div>
             </div>
-            <div className="card-content">
-              <p>Find new routines, exercises, tips and tricks to keep you on track and getting stronger.</p>
-            </div>
-            <div className="card-action">
-              <Link to="/discover">Discover</Link>
+          </div>
+          <div className="col m12 l4">
+            <div className="card">
+              <div className="card-image">
+                <img src={image3} alt="weight plates" />
+                <span className="card-title">Discover</span>
+              </div>
+              <div className="card-content">
+                <p>Find new routines, exercises, tips and tricks to keep you on track and getting stronger.</p>
+              </div>
+              <div className="card-action">
+                <Link to="/discover">Discover</Link>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-
-  );
+    );
+  }
 };
 
-export default Dashboard;
+Dashboard.propTypes = {
+  auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    auth: state.auth
+  }
+}
+
+export default connect(mapStateToProps)(Dashboard);
