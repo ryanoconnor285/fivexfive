4356f4bfea7a404a2b6b424d37017fd47475b7b7
rocsteady888 <rocsteady888@gmail.com>
2019-03-06 21:09:13 -0500

    added clearCurrentProfile when user logs out

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
+import { clearCurrentProfile } from './actions/profileActions';
 
 import { Provider } from 'react-redux';
 import store from './store'
@@ -24,18 +25,21 @@ import './App.css';
 
 // Check for token
 if (localStorage.jwtToken) {
-  // Set the auth token header auth
+  // Set auth token header auth
   setAuthToken(localStorage.jwtToken);
-  // Decode Token and get user infor and expiration
+  // Decode token and get user info and exp
   const decoded = jwt_decode(localStorage.jwtToken);
   // Set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
 
   // Check for expired token
-  const currentTime = Date.now() /1000;
-  if(decoded.exp < currentTime) {
-    // Logout User
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
     store.dispatch(logoutUser());
+    // Clear user's profile
+    store.dispatch(clearCurrentProfile());
+    // Redirect to login
     window.location.href = '/login';
   }
 }
