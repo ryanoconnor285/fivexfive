a3ab68befe355c9ab17cfe62f53a8af296d4dbb8
rocsteady888 <rocsteady888@gmail.com>
2019-03-07 20:36:47 -0500

    intial commit

--- client/src/components/common/Preloader.js
+++ client/src/components/common/Preloader.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Preloader = () => {
   return (
-    <div class="preloader-wrapper big active">
-      <div class="spinner-layer spinner-blue">
-        <div class="circle-clipper left">
-          <div class="circle"></div>
-        </div><div class="gap-patch">
-          <div class="circle"></div>
-        </div><div class="circle-clipper right">
-          <div class="circle"></div>
+    <div className="preloader-wrapper big active">
+      <div className="spinner-layer spinner-blue">
+        <div className="circle-clipper left">
+          <div className="circle"></div>
+        </div><div className="gap-patch">
+          <div className="circle"></div>
+        </div><div className="circle-clipper right">
+          <div className="circle"></div>
         </div>
       </div> 
     </div>
